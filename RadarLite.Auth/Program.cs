14f486dba0274f4bd99ecf92d2afbea5cc34cf7c
jtplautz <jimmieplautz@gmail.com>
2022-05-02 00:17:34 -0500

    identiy framework start

---
+++ RadarLite.Auth/Program.cs
@@ -0,0 +1,39 @@
+using IdentityServerAspNetIdentity;
+using Serilog;
+
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .WriteTo.Seq(builder.Configuration.GetConnectionString("Seq"))
+        .Enrich.FromLogContext()
+        .ReadFrom.Configuration(ctx.Configuration));
+
+    Log.Information("RadarLite.Auth starting up...");
+
+    var app = builder
+        .ConfigureServices()
+        .ConfigurePipeline();
+
+    // this seeding is only for the template to bootstrap the DB and users.
+    // in production you will likely want a different approach.
+    if (args.Contains("/seed"))
+    {
+        Log.Information("Seeding database...");
+        SeedData.EnsureSeedData(app);
+        Log.Information("Done seeding database. Exiting.");
+        return;
+    }
+
+    app.Run();
+}
+catch (Exception ex) when (ex.GetType().Name is not "StopTheHostException") // https://github.com/dotnet/runtime/issues/60600
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
---
+++ RadarLite.Auth/README.txt
@@ -0,0 +1,5 @@
+﻿Vue.ts -> IdentityServer that uses microsoft aspnet identity. 
+
+1. Get the Vue client correctly configured
+2. Configure the Identity server to correctly issue auth
+3. Replace in memory data using SQL and Microsoft Identity.
